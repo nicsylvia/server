a28ad52ccfd497f753d374e9f9e68d0e61a1cd98
nicsylvia <nicsylvia15f@gmail.com>
2023-03-27 19:45:47 -0700

    removed dist

--- dist/Middlewares/BusinessValidation/BusinessValidation.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.BusinessLoginValidation = exports.BusinessRegisterValidation = void 0;
-const Validator_1 = require("../Validator");
-const BusinessSchema_1 = require("./BusinessSchema");
-const BusinessRegisterValidation = (req, res, next) => (0, Validator_1.Validator)(BusinessSchema_1.BusinessSchemaValidation.Register, req.body, next);
-exports.BusinessRegisterValidation = BusinessRegisterValidation;
-const BusinessLoginValidation = (req, res, next) => (0, Validator_1.Validator)(BusinessSchema_1.BusinessSchemaValidation.Login, req.body, next);
-exports.BusinessLoginValidation = BusinessLoginValidation;
